4e5a45f924df612e556d48cb47e4b286a8550942
BagusRezky <bagusrezky15@gmail.com>
2024-05-23 02:22:44 +0700

    feat: add admin dashboard screen
    This commit adds the admin dashboard screen, which displays a placeholder widget.

---
+++ lib/screens/admin/dashboard_admin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DashboardAdmin extends StatelessWidget {
+  const DashboardAdmin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
