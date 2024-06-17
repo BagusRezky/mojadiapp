46cb820f04f17aa1c78a542f93b9f3ce01e40871
satianferdy <satianferdy@gmail.com>
2024-06-17 16:57:29 +0700

    dinata: profile & statistik

---
+++ lib/screens/profile/update_profile.dart
@@ -0,0 +1,234 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+
+const String tProfileImage = "assets/male.png";
+
+class UpdateProfileScreen extends StatefulWidget {
+  final DocumentSnapshot userProfile;
+
+  const UpdateProfileScreen({
+    required this.userProfile,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _UpdateProfileScreenState createState() => _UpdateProfileScreenState();
+}
+
+class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController birthDateController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fullNameController.text = widget.userProfile['fullName'] ?? '';
+    emailController.text = widget.userProfile['email'] ?? '';
+    birthDateController.text = widget.userProfile['birthDate'] ?? '';
+    addressController.text = widget.userProfile['address'] ?? '';
+  }
+
+  void _selectDate() async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        birthDateController.text =
+            "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
+  }
+
+  void _saveProfile() {
+    String fullName = fullNameController.text;
+    String birthDate = birthDateController.text;
+    String address = addressController.text;
+
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.userProfile.id)
+        .update({
+      'fullName': fullName,
+      'birthDate': birthDate,
+      'address': address,
+    }).then((_) {
+      Navigator.pop(context, {
+        'fullName': fullName,
+        'birthDate': birthDate,
+        'address': address,
+      });
+    }).catchError((error) {
+      print("Failed to update user profile: $error");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Update Profile',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w600,
+            fontSize: 24.sp,
+            height: 1.5,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: const Image(image: AssetImage(tProfileImage)),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  width: 70,
+                  height: 35,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.yellow),
+                  child: const Icon(
+                    LineAwesomeIcons.camera_solid,
+                    color: Colors.black,
+                    size: 25,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: fullNameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        labelText: 'Full Name',
+                        prefixIcon: const Icon(LineAwesomeIcons.user),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                          borderSide:
+                              const BorderSide(width: 2, color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        labelText: 'Email',
+                        prefixIcon: const Icon(LineAwesomeIcons.envelope),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                          borderSide:
+                              const BorderSide(width: 2, color: Colors.black),
+                        ),
+                      ),
+                      readOnly: true, // Mark as readOnly if not editable
+                    ),
+                    const SizedBox(height: 12),
+                    TextFormField(
+                      controller: birthDateController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        labelText: 'Birth Date',
+                        hintText: 'DD/MM/YYYY',
+                        prefixIcon: const Icon(Icons.calendar_today),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                          borderSide:
+                              const BorderSide(width: 2, color: Colors.black),
+                        ),
+                      ),
+                      onTap: _selectDate,
+                      readOnly: true,
+                    ),
+                    const SizedBox(height: 12),
+                    TextFormField(
+                      controller: addressController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        labelText: 'Address',
+                        prefixIcon: Icon(LineAwesomeIcons.city_solid),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                          borderSide:
+                              const BorderSide(width: 2, color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: _saveProfile,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          side: BorderSide.none,
+                          shape: const StadiumBorder(),
+                        ),
+                        child: const Text("Confirm",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: double.infinity,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/profile');
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 12),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: const Text(
+                            "Cancel",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/statistik/statistik.dart
@@ -0,0 +1,301 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Statistik extends StatefulWidget {
+  const Statistik({super.key});
+
+  @override
+  _StatistikState createState() => _StatistikState();
+}
+
+class _StatistikState extends State<Statistik> {
+  String selectedYear = '2024';
+  Map<String, double> categoryData = {};
+  Map<int, Map<String, int>> monthlyCounts = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPieChartData();
+    _fetchBarChartData();
+  }
+
+  Future<void> _fetchPieChartData() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('reports').get();
+    Map<String, int> categoryCounts = {};
+
+    for (var doc in querySnapshot.docs) {
+      String category = doc['kategori'];
+      categoryCounts[category] = (categoryCounts[category] ?? 0) + 1;
+    }
+
+    int total = categoryCounts.values.fold(0, (sum, count) => sum + count);
+
+    setState(() {
+      categoryData = categoryCounts
+          .map((key, value) => MapEntry(key, (value / total) * 100));
+    });
+  }
+
+  Future<void> _fetchBarChartData() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('reports').get();
+
+    for (var doc in querySnapshot.docs) {
+      DateTime date;
+      if (doc['tanggal'] is Timestamp) {
+        date = (doc['tanggal'] as Timestamp).toDate();
+      } else {
+        date = DateTime.parse(doc['tanggal']);
+      }
+      int month = date.month;
+
+      if (doc['status_list'] is List<dynamic>) {
+        List<dynamic> statusList = doc['status_list'];
+        for (var status in statusList) {
+          if (status is Map<String, dynamic> && status.containsKey('status')) {
+            String statusValue = status['status'];
+            if (!monthlyCounts.containsKey(month)) {
+              monthlyCounts[month] = {
+                'Belum Mulai': 0,
+                'Proses': 0,
+                'Selesai': 0
+              };
+            }
+            monthlyCounts[month]![statusValue] =
+                (monthlyCounts[month]![statusValue] ?? 0) + 1;
+          }
+        }
+      }
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Statistik',
+          textAlign: TextAlign.center,
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w600,
+            fontSize: 24.sp,
+            height: 1.5,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(35.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 180,
+                  height: 150,
+                  child: AspectRatio(
+                    aspectRatio: 1.2,
+                    child: PieChart(
+                      PieChartData(
+                        sections: _buildSections(),
+                        borderData: FlBorderData(show: false),
+                        sectionsSpace: 3,
+                        centerSpaceRadius: 50,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                _buildLegend(),
+              ],
+            ),
+            const SizedBox(height: 30),
+            DropdownButton<String>(
+              value: selectedYear,
+              items: <String>['2024', '2025', '2026']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text('Tahun: $value'),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedYear = newValue!;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: selectedYear == '2024'
+                  ? _buildBarChart()
+                  : Center(
+                      child: Text(
+                        "Belum ada grafik untuk tahun yang dipilih",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<PieChartSectionData> _buildSections() {
+    return categoryData.entries.map((entry) {
+      return PieChartSectionData(
+        value: entry.value,
+        color: _getColorForCategory(entry.key),
+        title: '${entry.value.toStringAsFixed(1)}%',
+        titleStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      );
+    }).toList();
+  }
+
+  Color _getColorForCategory(String category) {
+    switch (category) {
+      case 'Pelanggaran Lingkungan':
+        return Colors.blue;
+      case 'Kerusakan Fasilitas Sosial':
+        return Colors.green;
+      case 'Kerusakan Jalan':
+        return Colors.orange;
+      case 'Kerusakan Drainase':
+        return Colors.grey;
+      default:
+        return Colors.black;
+    }
+  }
+
+  Widget _buildLegend() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: categoryData.keys.map((category) {
+          return _buildLegendItem(_getColorForCategory(category), category);
+        }).toList());
+  }
+
+  Widget _buildLegendItem(Color color, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Container(
+            width: 10,
+            height: 14,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: color,
+            ),
+          ),
+          const SizedBox(width: 8),
+          Text(
+            title,
+            style: TextStyle(fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+
+  BarChart _buildBarChart() {
+    return BarChart(
+      BarChartData(
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 10,
+        barTouchData: BarTouchData(enabled: false),
+        titlesData: FlTitlesData(
+          show: true,
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (double value, TitleMeta meta) {
+                switch (value.toInt()) {
+                  case 1:
+                    return Text('Jan');
+                  case 2:
+                    return Text('Feb');
+                  case 3:
+                    return Text('Mar');
+                  case 4:
+                    return Text('Apr');
+                  case 5:
+                    return Text('May');
+                  case 6:
+                    return Text('Jun');
+                  case 7:
+                    return Text('Jul');
+                  case 8:
+                    return Text('Aug');
+                  case 9:
+                    return Text('Sep');
+                  case 10:
+                    return Text('Oct');
+                  case 11:
+                    return Text('Nov');
+                  case 12:
+                    return Text('Dec');
+                  default:
+                    return Text('');
+                }
+              },
+            ),
+          ),
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 1,
+              getTitlesWidget: (double value, TitleMeta meta) {
+                return Text(value.toInt().toString());
+              },
+            ),
+          ),
+        ),
+        borderData: FlBorderData(show: false),
+        barGroups: _buildBarGroups(),
+      ),
+    );
+  }
+
+  List<BarChartGroupData> _buildBarGroups() {
+    return monthlyCounts.entries.map((entry) {
+      int month = entry.key;
+      Map<String, int> counts = entry.value;
+
+      return BarChartGroupData(
+        x: month,
+        barRods: [
+          BarChartRodData(
+            toY: counts['Belum Mulai']?.toDouble() ?? 0,
+            color: Colors.red,
+          ),
+          BarChartRodData(
+            toY: counts['Proses']?.toDouble() ?? 0,
+            color: Colors.yellow,
+          ),
+          BarChartRodData(
+            toY: counts['Selesai']?.toDouble() ?? 0,
+            color: Colors.green,
+          ),
+        ],
+      );
+    }).toList();
+  }
+}
